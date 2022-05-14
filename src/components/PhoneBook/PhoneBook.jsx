a417491e093d75115ded9d4dd491cd2c7b6c862f
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-05-14 09:05:48 +0300

    add localstoradge

--- src/components/PhoneBook/PhoneBook.jsx
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -6,18 +6,34 @@ import { Contacts } from './Contacts'
 import { Filter } from "../Filter";
 import shortid from "shortid";
 
-const CONTACT_LIST = [
-  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-];
+// const CONTACT_LIST = [
+//   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+//   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+//   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+//   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+// ];
 
 
 export class PhoneBook extends Component{
   state = {
-    contacts: [...CONTACT_LIST],
+    contacts: [],
     filter: '',
+  }
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parseContacts = JSON.parse(contacts);
+    // console.log(parseContacts);
+    if (parseContacts) {
+      this.setState({contacts: parseContacts})
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      // console.log('apdate contacts');
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
   }
 
   addContact = (name, number) => {
